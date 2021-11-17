dea411e02c23361ad1422458d8230e1298d33e36
Jonathan Choi <jonchoi11@gmail.com>
2021-11-17 15:23:16 -0700

    add canvas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React, { useLayoutEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Painting</h1>
-    </div>
+    <canvas
+      id="canvas"
+      style={{ backgroundColor: "blue" }}
+      width={window.innerWidth}
+      height={window.innerHeight}
+    >
+      Canvas
+    </canvas>
   );
 }
 
