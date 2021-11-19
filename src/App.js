977c93210bb0251037ab36295f1331fd4fe9afdc
Jonathan Choi <jonchoi11@gmail.com>
2021-11-19 16:56:38 -0700

    add line drawing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React, { useLayoutEffect, useState } from "react";
 import "./App.css";
+import rough from "roughjs/bundled/rough.esm";
+
+const generator = rough.generator();
 
 function App() {
   useLayoutEffect(() => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
 
-    ctx.fillStyle = "green";
-    ctx.fillRect(10, 10, 150, 100);
+    const roughCanvas = rough.canvas(canvas);
+    const rect = generator.rectangle(10, 10, 100, 100);
+    const line = generator.line(10, 10, 110, 110);
+
+    roughCanvas.draw(rect);
+    roughCanvas.draw(line);
   });
 
   return (
-    <canvas
-      id="canvas"
-      style={{ backgroundColor: "blue" }}
-      width={window.innerWidth}
-      height={window.innerHeight}
-    >
+    <canvas id="canvas" width={window.innerWidth} height={window.innerHeight}>
       Canvas
     </canvas>
   );
