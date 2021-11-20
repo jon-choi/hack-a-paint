3d2d8463919e047528322781d8796c9cd9b14147
Jonathan Choi <jonchoi11@gmail.com>
2021-11-19 17:05:40 -0700

    add mouse events

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import rough from "roughjs/bundled/rough.esm";
 const generator = rough.generator();
 
 function App() {
+  const [elements, setElements] = useState([]);
+
+  const [drawing, setDrawing] = useState(false);
+
   useLayoutEffect(() => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
@@ -16,9 +20,27 @@ function App() {
     roughCanvas.draw(rect);
     roughCanvas.draw(line);
   });
+  const handleMouseDown = (event) => {
+    setDrawing(true);
+  };
+  const handleMouseMove = (event) => {
+    if (!drawing) return;
 
+    const { clientX, clientY } = event;
+    console.log(clientX, clientY);
+  };
+  const handleMouseUp = () => {
+    setDrawing(false);
+  };
   return (
-    <canvas id="canvas" width={window.innerWidth} height={window.innerHeight}>
+    <canvas
+      id="canvas"
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+    >
       Canvas
     </canvas>
   );
