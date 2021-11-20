448e82daa53e73a0611a42f4c7ed21c503de06ea
Jonathan Choi <jonchoi11@gmail.com>
2021-11-20 16:26:37 -0700

    add rough elements

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import rough from "roughjs/bundled/rough.esm";
 
 const generator = rough.generator();
 
+function createElement(x1, y1, x2, y2) {
+  const roughElement = generator.line(x1, y1, x2, y2);
+  return { x1, y1, x2, y2, roughElement };
+}
+
 function App() {
   const [elements, setElements] = useState([]);
 
@@ -15,24 +20,36 @@ function App() {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     const roughCanvas = rough.canvas(canvas);
-    const rect = generator.rectangle(10, 10, 100, 100);
-    const line = generator.line(10, 10, 110, 110);
 
-    roughCanvas.draw(rect);
-    roughCanvas.draw(line);
-  });
+    elements.forEach(({ roughElement }) => roughCanvas.draw(roughElement));
+  }, [elements]);
+
   const handleMouseDown = (event) => {
     setDrawing(true);
+
+    const { clientX, clientY } = event;
+
+    const element = createElement(clientX, clientY, clientX, clientY);
+    setElements((prevState) => [...prevState, element]);
   };
+
   const handleMouseMove = (event) => {
     if (!drawing) return;
 
     const { clientX, clientY } = event;
-    console.log(clientX, clientY);
+    const index = elements.length - 1;
+    const { x1, y1 } = elements[index];
+    const updatedElement = createElement(x1, y1, clientX, clientY);
+
+    const elementsCopy = [...elements];
+    elementsCopy[index] = updatedElement;
+    setElements(elementsCopy);
   };
+
   const handleMouseUp = () => {
     setDrawing(false);
   };
+
   return (
     <canvas
       id="canvas"
