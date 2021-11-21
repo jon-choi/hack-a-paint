993b25941c900af5c3040fab25f96ade0541d3d2
Jonathan Choi <jonchoi11@gmail.com>
2021-11-20 21:14:01 -0700

    add styles and set width and height for panel

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -3,8 +3,8 @@ import React, { useState } from "react";
 import "../styles/editor.scss";
 
 export default function Editor() {
-  const [panelWidth, setPanelWidth] = useState(20);
-  const [panelHeight, setPanelHeight] = useState(20);
+  const [panelWidth, setPanelWidth] = useState(16);
+  const [panelHeight, setPanelHeight] = useState(16);
   const [hideOptions, setHideOptions] = useState(false);
   const [hideDrawingPanel, setHideDrawingPanel] = useState(true);
   const [buttonText, setButtonText] = useState("start drawing");
@@ -16,15 +16,29 @@ export default function Editor() {
       <h2>Enter Dimensions</h2>
       <div id="options">
         <div className="option">
-          <input type="number" className="PanelInput" defaultValue="20" />
+          <input
+            type="number"
+            className="PanelInput"
+            defaultValue={panelWidth}
+            onChange={(e) => {
+              setPanelWidth(e.target.value);
+            }}
+          />
           <span>Width</span>
         </div>
         <div className="option">
-          <input type="number" className="PanelInput" defaultValue="20" />
+          <input
+            type="number"
+            className="PanelInput"
+            defaultValue={panelHeight}
+            onChange={(e) => {
+              setPanelHeight(e.target.value);
+            }}
+          />
           <span>Height</span>
         </div>
       </div>
-      <button className="button">Start Drawing</button>
+      <button className="button">{buttonText}</button>
     </div>
   );
 }
