54b2036a27db03696b3575ae6f658d560df1d3b0
Jonathan Choi <jonchoi11@gmail.com>
2021-11-22 13:20:53 -0700

    create header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import "../styles/App.scss";
 import Editor from "./Editor";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Editor />
     </div>
   );
