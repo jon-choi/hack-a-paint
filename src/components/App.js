eabb414d73de0d0deb3c39b0878916caa6b6f9a4
Jonathan Choi <jonchoi11@gmail.com>
2021-11-22 16:18:10 -0700

    figure out landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "../styles/App.scss";
 import Editor from "./Editor";
 import LandingPage from "./LandingPage";
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,13 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/landingPage.scss";
 
 const LandingPage = () => {
   return (
-    <div>
-      Welcome to the landing page for pixe-paint
-      <button className="button">
-        <Link to="/editor">Start Drawing</Link>
-      </button>
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Welcome to the wonderful world of Pixe-Paint!
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+
+        <div className="gpt3__header-content__people">
+          {/* <img src={people} /> */}
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+
+      <div className="gpt3__header-image">{/* <img src={ai} /> */} sup</div>
     </div>
   );
 };
